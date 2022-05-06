8b4b3c6926b867e9c89a0e793a7a1d2993a32e4b
Khgaa <101567770+Khgaa@users.noreply.github.com>
2022-05-06 09:46:53 -0300

    Add files via upload

---
+++ MySQL/Script escola.sql
@@ -0,0 +1,18 @@
+create database salaDeAula;
+
+create table Turma(
+        id_Turma int NOT NULL auto_increment,
+        nome_Turma varchar(50) NOT NULL
+);
+
+
+create table Aluno(
+	    id_Aluno int NOT NULL auto_increment primary key,
+        nome varchar(50) NOT NULL,
+        endereco varchar(100) NOT NULL,
+        cpf varchar(20) NOT NULL,
+        rg varchar(20) NOT NULL,
+        id_turma int NOT NULL,
+        constraint fk_idturma_aluno foreign key(id_turma)
+        references Turma(id_turma)
+);
